677149e6b2dd7386058ecd25ddcab4ad15358ebb
moses <mor_95@163.com>
2022-04-01 16:47:21 +0800

    init

---
+++ example.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	pc "github.com/mosesyu95/procControl"
+)
+
+
+func main() {
+	sema := pc.NewProcControl(10,100)
+	for i := 0; i < 100; i++ {
+		fmt.Println("start ",i)
+		sema.Acquire()
+		go func(i int) {
+			defer func() {
+				sema.Release()
+			}()
+			time.Sleep(1 * time.Second)
+			fmt.Println("print ",i)
+		}(i)
+	}
+	sema.Wait()
+}
---
+++ procControl.go
@@ -0,0 +1,32 @@
+package procControl
+
+import (
+	"sync"
+)
+
+type ProcControl struct {
+	wg      sync.WaitGroup
+	channel chan int8
+}
+
+func NewProcControl(procNum, totalProc int) *ProcControl {
+	wg := sync.WaitGroup{}
+	wg.Add(totalProc)
+	return &ProcControl{
+		wg:      wg,
+		channel: make(chan int8, procNum),
+	}
+}
+
+func (p *ProcControl) Acquire() {
+	p.channel <- int8(0)
+}
+
+func (p *ProcControl) Release() {
+	p.wg.Done()
+	<-p.channel
+}
+
+func (p *ProcControl) Wait() {
+	p.wg.Wait()
+}
